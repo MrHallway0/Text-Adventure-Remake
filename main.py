13e8f11962d9c34eef7de57f2611c79db2427c17
Mr. Hallway <65696362+MrHallway0@users.noreply.github.com>
2020-10-22 17:40:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,37 @@ Version   | Alpha 0.1
 ____________________________________
 '''
 
+# Set settings to defualt
+friends = 0
+
+# add/remove to friends score
+def addFriend(int(num)):
+  try:
+    friends = friends + num
+    print("You now have " + str(friends) + "!")
+  except:
+    print("DEBUG: ERROR could not convert `num` to int check that it's given in all times it's told to run or that it's not messed with... Adding one to `friends`")
+    friends = friends + 1
+    print("You now have " + str(friends) + "!")
+
 # Get things that are needed!
 from time import sleep as wait
 
 # Death actions
 def deadForGood():
   print("You go home. Plop down and die at an old age...")
+
+# Break in text
+def break(time):
+  try:
+    time.sleep(time)
+  except:
+    time.sleep(0.5)
+  print("")
+
+# Y,N Question define
+def yn(str(q)):
+  answer = str(input(q + " (y,n)")).lower()
 
 # Ask for username
 name = input('''Welcome! Please enter your name...
@@ -56,7 +81,29 @@ goQ = input('''You have the choice to go... (y,n)
 ''').lower()
 
 if goQ == "y":
-  print("You go...")
+  print("You pack your bags get into your small beetle and drive on")
+  break(0)
+  for i in range(0,3):
+    print("Driving...")
+    break(0.25)
+  
+  print("It's late by the time you get to the barn. Although the people in the town heard a car, witch doesn't happen often.")
+  break()
+  print("You are greeted.")
+  yn("Do you leave them and just go inside the home?")
+  if answer("y"):
+    addFriend(3)
+    
+    print("You are spending time with your new friends...")
+    break(2)
+  
+  else:
+    addFriends(0)
+    
+  print("You sleep through the night...")
+  break(2)
 
 else:
   deadForGood()
+
+print("THANK YOU FOR USING THE BETA! I have more to the story it's just not added yet! :)")
